4f3cbda4d18c0aecf2d079da63e50ff160ab4479
DESKTOP-DMBBVHN\nocturno <javiermder@gmail.com>
2025-10-24 20:52:36 +0200

    Controles

--- 04ControlesBasicos/app/src/main/java/com/pmdm/a04controlesbasicos/MainActivity3Ejercicio2.java
+++ 04ControlesBasicos/app/src/main/java/com/pmdm/a04controlesbasicos/MainActivity3Ejercicio2.java
@@ -1,6 +1,8 @@
 package com.pmdm.a04controlesbasicos;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3Ejercicio2 extends AppCompatActivity {
 
+    Spinner spinner ;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3_ejercicio2);
+
+        spinner = findViewById(R.id.spinner2);
+
+        String[] paises = {"España","Andorra","Portugal","Alemania","Suecia"};
+        spinner.setAdapter( new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,paises));
+
+
 
     }
 }
